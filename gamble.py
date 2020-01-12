95b6291a81bfd8be18612d9b9e72029230bab165
Pinghao Qi <pinghaoq@gmail.com>
2020-01-11 21:42:38 -0500

    Adding plotting

--- gamble.py
+++ gamble.py
@@ -1,18 +1,27 @@
 import random
+import matplotlib.pyplot as plt 
 
-roll_number = 2
+plot_roll_start = 2
+plot_roll_end = 15
 num_games = 1000000
-first_player_num_wins = 0
+x = []
+y = []
+for roll_number in range(plot_roll_start, plot_roll_end + 1):
+    first_player_num_wins = 0
+    for i in range(num_games):
+        last_rolled_number = roll_number
+        while True:
+            last_rolled_number = random.randrange(last_rolled_number) + 1
+            if last_rolled_number == 1:
+                break
+            last_rolled_number = random.randrange(last_rolled_number) + 1
+            if last_rolled_number == 1:
+                first_player_num_wins += 1
+                break
+    x.append(roll_number)
+    y.append(1- (float(first_player_num_wins) / num_games))
 
-for i in range(num_games):
-    last_rolled_number = roll_number
-    while True:
-        last_rolled_number = random.randrange(last_rolled_number) + 1
-        if last_rolled_number == 1:
-            break
-        last_rolled_number = random.randrange(last_rolled_number) + 1
-        if last_rolled_number == 1:
-            first_player_num_wins += 1
-            break
-        
-print(f'Out of {num_games}, player rolling first won {first_player_num_wins}, {float(first_player_num_wins)/num_games * 100}%')
+plt.plot(x, y)
+plt.xlabel('Start roll')
+plt.ylabel('p2 win prob')
+plt.show()
